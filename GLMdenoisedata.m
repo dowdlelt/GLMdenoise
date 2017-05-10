e4d7c4311df5125da63cc5ee7828aaf5c2d4e545
dowdlelt <logan.dowdle@gmail.com>
2017-05-10 10:51:08 -0400

    better single conv for mask
    Fixed typo, single conversion for reconmask moved outside of auto_vec
    conditional.

--- GLMdenoisedata.m
+++ GLMdenoisedata.m
@@ -477,13 +477,15 @@ if any(flatten(~isfinite(data{1})))
   fprintf('*** GLMdenoisedata: WARNING: we checked the first run and found some non-finite values (e.g. NaN, Inf). unexpected results may occur due to non-finite values. please fix and re-run GLMdenoisedata. ***\n');
 end
 
+if isfield(opt,'reconmask') %Convert mask to single, so figures work properly.
+    if ~isa(opt.reconmask,'single')
+        fprintf('*** GLMdenoisedata: converting mask to single...\n');
+        opt.reconmask = single(opt.reconmask);
+    end  
+end
+
 % if opt.reconmask is provided, vectorize and remove zeros
 if isfield(opt,'reconmask') && size(data{1},4) >1
-    if ~isa(opt.reconmask,'single')
-        fprintf('*** GLMdenoisedata: converting converting mask to single...\n');
-        opt.reconmask = single(opt.reconmask);
-    end
-    
     numvoxels = size(reshape(opt.reconmask, [],1),1);
     fprintf('*** GLMdenoisedata: vectorizing data.');
     mask_ind = opt.reconmask(:,:,:,1) ~= 0; % generate the indices where data live
